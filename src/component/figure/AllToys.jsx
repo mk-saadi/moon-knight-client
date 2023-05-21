94d9447c0002e83af4e3c6ccaa11705f21adb7a5
mk-saadi <mksaadi820@gmail.com>
2023-05-21 20:12:03 +0600

    sort

--- src/component/figure/AllToys.jsx
+++ src/component/figure/AllToys.jsx
@@ -1,10 +1,12 @@
 import { useLoaderData } from "react-router-dom";
 import AllToysD from "./AllToysD";
 import { useState } from "react";
+import { TbArrowsSort } from "react-icons/tb";
 
 const AllToys = () => {
     const figs = useLoaderData();
     const [searchQuery, setSearchQuery] = useState("");
+    const [sortOrder, setSortOrder] = useState("asc");
 
     const filteredFigs = figs.filter((fig) =>
         fig.name.toLowerCase().includes(searchQuery.toLowerCase())
@@ -13,6 +15,18 @@ const AllToys = () => {
     const handleSearch = (e) => {
         setSearchQuery(e.target.value);
     };
+
+    // const sortedFigs = filteredFigs.sort((a, b) => a.price - b.price);
+
+    const handleSort = () => {
+        const newSortOrder = sortOrder === "asc" ? "desc" : "asc";
+        setSortOrder(newSortOrder);
+    };
+
+    const sortedFigs = filteredFigs.sort((a, b) => {
+        const sortFactor = sortOrder === "asc" ? 1 : -1;
+        return sortFactor * (a.price - b.price);
+    });
 
     return (
         <>
@@ -25,13 +39,22 @@ const AllToys = () => {
                     className="input input-bordered w-full"
                 />
             </div>
+            <div className="mb-4">
+                <button
+                    onClick={handleSort}
+                    className="btn btn-accent rounded-sm"
+                >
+                    Sort
+                    <TbArrowsSort /> {sortOrder === "asc" ? "" : ""}
+                </button>
+            </div>
             <div
                 style={{ width: "98vw" }}
                 className="my-16 overflow-x-auto min-h-screen mx-auto"
             >
                 <table className="table table-zebra w-full">
                     <tbody>
-                        {filteredFigs
+                        {sortedFigs
                             .map((fig) => (
                                 <AllToysD
                                     key={fig._id}
