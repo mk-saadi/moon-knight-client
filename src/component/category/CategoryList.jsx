65cea9beed07f39c3186c59a76f45c4e44223d83
mk-saadi <mksaadi820@gmail.com>
2023-05-23 01:57:34 +0600

    post assignment

--- src/component/category/CategoryList.jsx
+++ src/component/category/CategoryList.jsx
@@ -2,39 +2,46 @@ import { useState, useEffect } from "react";
 
 const CategoryList = ({ figures, onCategorySelect }) => {
     const categoryNames = Array.from(new Set(figures.map((fig) => fig.category)));
-    const [selectedCategory, setSelectedCategory] = useState(null);
+    const [selectedCategory, setSelectedCategory] = useState(categoryNames[0]);
 
     useEffect(() => {
-        const storedCategory = localStorage.getItem("selectedCategory");
-        if (storedCategory && categoryNames.includes(storedCategory)) {
-            setSelectedCategory(storedCategory);
-            onCategorySelect(storedCategory);
-        } else {
-            // Set the initial selected category
-            const initialCategory = categoryNames[0];
-            setSelectedCategory(initialCategory);
-            onCategorySelect(initialCategory);
-        }
-    }, [categoryNames, onCategorySelect]);
+        onCategorySelect(selectedCategory);
+    }, [selectedCategory, onCategorySelect]);
 
     const handleCategoryClick = (category) => {
         setSelectedCategory(category);
-        localStorage.setItem("selectedCategory", category);
-        onCategorySelect(category);
     };
+
+    useEffect(() => {
+        if (!selectedCategory) {
+            const initialCategory = categoryNames[0];
+            setSelectedCategory(initialCategory);
+        }
+    }, [selectedCategory, categoryNames]);
 
     return (
         <div className="grid grid-cols-2 md:grid-cols-5 md:gap-4 gap-2 md:mr-40 justify-left mt-16 mb-6 mx-4 md:ml-24 sm:mx-20">
             {categoryNames.map((category) => (
-                <button
+                <div
                     key={category}
-                    className={`btn text-white btn-xs text-xs md:text-base md:btn-md rounded-sm no-animation ${
-                        selectedCategory === category ? "btn-accent text-white" : "btn-info"
+                    className={`btn-xs text-xs sm:text-sm md:text-base cursor-pointer border border-b-0 border-t-4 border-x-0 flex flex-nowrap mb-4  ${
+                        selectedCategory === category
+                            ? "border-info text-info"
+                            : "border-transparent text-accent"
                     }`}
                     onClick={() => handleCategoryClick(category)}
                 >
                     {category}
-                </button>
+                </div>
+                // <button
+                //     key={category}
+                //     className={`btn text-white btn-xs text-xs sm:text-sm md:btn-md rounded-sm no-animation ${
+                //         selectedCategory === category ? "btn-accent text-white" : "btn-info"
+                //     }`}
+                //     onClick={() => handleCategoryClick(category)}
+                // >
+                //     {category}
+                // </button>
             ))}
         </div>
     );
